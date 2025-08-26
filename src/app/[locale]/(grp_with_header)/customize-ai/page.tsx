5394d9337b1000acd821383fc063c531403b0c96
Moustafa <moustafaadel097@gmail.com>
2025-08-26 23:25:13 +0300

    mohamed essam request header and footer ai page

---
+++ src/app/[locale]/(grp_with_header)/customize-ai/page.tsx
@@ -0,0 +1,14 @@
+import AiDriven from "@/components/UI/AIDriven/AiDriven"
+import React from "react"
+
+function page() {
+  return (
+    <>
+      <div style={{ marginTop: "110px" }}>
+        <AiDriven />
+      </div>
+    </>
+  )
+}
+
+export default page
